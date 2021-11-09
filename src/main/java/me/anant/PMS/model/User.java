684aa0affa111837c5e26ea4d796528064ad5a10
Luiz Carlos <66796526+luiz-carlos-1985@users.noreply.github.com>
2021-11-09 10:57:41 -0300

    Update User.java

--- src/main/java/me/anant/PMS/model/User.java
+++ src/main/java/me/anant/PMS/model/User.java
@@ -19,17 +19,19 @@ public class User {
 	@Column(nullable=false, unique=true)
 	String email;
 	String password;
-    String role;
+    	String role;
 	@OneToMany(mappedBy = "user",cascade = CascadeType.ALL)		
 	
 	Set<Order> orders;
 	public User() {
 		
 	}
+	
 	public User(long id) {
 		super();
 		this.id = id;
 	}
+	
 	public User(String name, String email, String password, String role) {
 		super();
 		this.name = name;
@@ -37,40 +39,53 @@ public class User {
 		this.password = password;
 		this.role = role;
 	}
+	
 	public long getId() {
 		return id;
 	}
+	
 	public void setId(long id) {
 		this.id = id;
 	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
 	public String getPassword() {
 		return password;
 	}
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
 	public String getRole() {
 		return role;
 	}
+	
 	public void setRole(String role) {
 		this.role = role;
 	}
+	
 	public Set<Order> getOrders() {
 		return orders;
 	}
+	
 	public void setOrders(Set<Order> orders) {
 		this.orders = orders;
 	}
+	
 }
